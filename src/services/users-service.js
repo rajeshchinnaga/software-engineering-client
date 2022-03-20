e94b949cecb77bdc5b120e73fb92c69a4fec78c5
adarshreddy87 <sparkadarshreddy@gmail.com>
2022-03-19 23:42:08 -0400

    Changes to tuit-list.test.js (Commented mocks at the beginning to make async tests work)

--- src/services/users-service.js
+++ src/services/users-service.js
@@ -5,33 +5,40 @@ const BASE_URL = "https://adarshreddy-se-node-a3.herokuapp.com/api";
 const LOGIN_API = `${BASE_URL}/login`;
 const USERS_API = `${BASE_URL}/users`;
 
-export const createUser = (user) =>
-  axios.post(`${USERS_API}`, user)
-    .then(response => response.data);
+export const createUser = (user) => {
+  return axios.post(`${USERS_API}`, user)
+      .then(response => response.data);
+}
 
-export const findAllUsers = () =>
-    axios.get(USERS_API)
-        .then(response => response.data);
+export const findAllUsers = () => {
+  return axios.get(USERS_API)
+      .then(response => response.data);
+}
 
-export const findUserById = (uid) =>
-    axios.get(`${USERS_API}/${uid}`)
-        .then(response => response.data);
+export const findUserById = (uid) => {
+  return axios.get(`${USERS_API}/${uid}`)
+      .then(response => response.data);
+}
 
-export const deleteUser = (uid) =>
-  axios.delete(`${USERS_API}/${uid}`)
-    .then(response => response.data);
+export const deleteUser = (uid) => {
+  return axios.delete(`${USERS_API}/${uid}`)
+      .then(response => response.data);
+}
 
-export const deleteUsersByUsername = (username) =>
-  axios.get(`${USERS_API}/username/${username}/delete`)
-    .then(response => response.data);
+export const deleteUsersByUsername = (username) => {
+  return axios.get(`${USERS_API}/username/${username}/delete`)
+      .then(response => response.data);
+}
 
-export const findUserByUsername = (username) =>
-    axios.get(`${USERS_API}/username/${username}/find`)
-        .then(response => response.data);
+export const findUserByUsername = (username) => {
+  return axios.get(`${USERS_API}/username/${username}/find`)
+      .then(response => response.data);
+}
 
-export const findUserByCredentials = (credentials) =>
-  axios.post(`${LOGIN_API}`, credentials)
-    .then(response => response.data);
+export const findUserByCredentials = (credentials) => {
+  return axios.post(`${LOGIN_API}`, credentials)
+      .then(response => response.data);
+}
 
 const service = {
   findAllUsers
--- src/tests/tuit-list.test.js
+++ src/tests/tuit-list.test.js
@@ -5,7 +5,7 @@ import {createTuit, deleteTuit, findAllTuits} from "../services/tuits-service";
 import axios from "axios";
 import {createUser, deleteUsersByUsername, findUserByUsername} from "../services/users-service";
 
-jest.mock('axios');
+// jest.mock('axios');
 
 const MOCKED_USERS = [
   "alice", "bob", "charlie"
@@ -47,63 +47,48 @@ describe('render from async', () => {
     password: 'adamsmith',
     email: 'adam@smith.com'
   };
-
   const sampleTuit = {
     tuit: 'Test Tuit 1',
   };
-
   let tids = [];
   let tuits = [];
   let newUser;
-
   beforeAll(async () => {
     await deleteUsersByUsername(adam.username);
     newUser = await createUser(adam);
-
     for (let tuitCount = 0; tuitCount < 5; tuitCount++) {
       const tuit = `Test Tuit ${tuitCount + 1}`;
       sampleTuit.tuit = tuit;
       sampleTuit.postedBy = newUser.username;
-
       let newTuit = await createTuit(newUser.username, sampleTuit);
-
       tids.push(newTuit._id);
       tuits.push(tuit);
     }
 
     return tids;
   });
-
   afterAll(() => {
     let promises = [];
-
     tids.forEach((tid) => {
       promises.push(deleteTuit(tid));
     });
     promises.push(deleteUsersByUsername(adam.username));
-
     return Promise.all(promises);
   });
-
   test('tuit list renders async', async () => {
     const allTuits = await findAllTuits();
     const user = await findUserByUsername(adam.username);
-
     const createdTuits = allTuits.filter(
         (tuit) => tids.indexOf(tuit._id) >= 0);
-
     const deleteATuit = (tid) =>
-            deleteTuit(tid)
+        deleteTuit(tid)
             .then();
-
     render(
         <HashRouter>
           <Tuits tuits={createdTuits} deleteTuit={deleteATuit}/>
         </HashRouter>);
-
     tuits.forEach((tuit) => {
       let linkElements = screen.getAllByText(tuit);
-
       linkElements.forEach((element) => {
         expect(element).toBeInTheDocument();
       })
